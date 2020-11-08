c0a5cbf7cd12eb58b690643fb4c343f8dc59199a
Nick Randell <nick.randell@sceneskope.com>
2020-11-08 09:24:24 +0000

    Change to use replace instead of replace all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ interface Props {
 const Index: React.FC<Props> = props => {
   const { data } = props
   const html = data.googleDocs.childMarkdownRemark.html
-  const cleanedHtml = html.replaceAll("http://./", "/")
+  const cleanedHtml = html.replace(new RegExp("http://./", "g"), "/")
   return (
     <div className="application">
       <Helmet>

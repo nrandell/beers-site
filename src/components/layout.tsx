8a591e5fad6297cfecff08680e1fb8da985fe8fb
nrandell <nick@nickrandell.com>
2021-02-26 07:04:21 +0000

    Update to try and cache

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap-reboot.css"
-import "fontsource-roboto/400-normal.css"
-import "fontsource-roboto/700-normal.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/700.css"
 import "./layout.css"
 
 export const Layout: React.FC = props => {
